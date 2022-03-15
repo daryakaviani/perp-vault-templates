756a2476026fa6371698ba7432a9a6576da73a0a
Darya Kaviani <daryakavi@gmail.com>
2022-03-15 14:09:24 -0700

    require we are buying eth otoken

--- contracts/utils/RollOverBase.sol
+++ contracts/utils/RollOverBase.sol
@@ -2,13 +2,13 @@
 pragma solidity >=0.7.2;
 pragma experimental ABIEncoderV2;
 
-import '@openzeppelin/contracts/access/Ownable.sol';
-import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import { SafeERC20 } from '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
-import { AirswapBase } from './AirswapBase.sol';
-import { IWhitelist } from '../interfaces/IWhitelist.sol';
-import { SwapTypes } from '../libraries/SwapTypes.sol';
+import {AirswapBase} from "./AirswapBase.sol";
+import {IWhitelist} from "../interfaces/IWhitelist.sol";
+import {SwapTypes} from "../libraries/SwapTypes.sol";
 
 /**
  * Error Codes
@@ -25,84 +25,90 @@ import { SwapTypes } from '../libraries/SwapTypes.sol';
  */
 
 contract RollOverBase is Ownable {
-  address public otoken;
-  address public nextOToken;
+    address public otoken;
+    address public nextOToken;
 
-  uint256 constant public MIN_COMMIT_PERIOD = 4 hours;
-  uint256 public commitStateStart;
+    uint256 public constant MIN_COMMIT_PERIOD = 4 hours;
+    uint256 public commitStateStart;
 
-/**
- * Idle: action will go "idle" after the vault closes this position & before the next oToken is committed.
- *
- * Committed: owner has already set the next oToken this vault is trading. During this phase, all funds are
- * already back in the vault and waiting for redistribution. Users who don't agree with the setting of the next
- * round can withdraw.
- *
- * Activated: after vault calls "rollover", the owner can start minting / buying / selling according to each action.
- */
-  enum ActionState {
-    Activated,
-    Committed,
-    Idle
-  }
+    /**
+     * Idle: action will go "idle" after the vault closes this position & before the next oToken is committed.
+     *
+     * Committed: owner has already set the next oToken this vault is trading. During this phase, all funds are
+     * already back in the vault and waiting for redistribution. Users who don't agree with the setting of the next
+     * round can withdraw.
+     *
+     * Activated: after vault calls "rollover", the owner can start minting / buying / selling according to each action.
+     */
+    enum ActionState {
+        Activated,
+        Committed,
+        Idle
+    }
 
-  ActionState public state;
-  IWhitelist public opynWhitelist;
+    ActionState public state;
+    IWhitelist public opynWhitelist;
 
-  function onlyCommitted() private view {
-    require(state == ActionState.Committed, "R1");
-  }
+    function onlyCommitted() private view {
+        require(state == ActionState.Committed, "R1");
+    }
 
-  function onlyActivated() internal view {
-    require(state == ActionState.Activated, "R2");
-  }
+    function onlyActivated() internal view {
+        require(state == ActionState.Activated, "R2");
+    }
 
+    function _initRollOverBase(address _opynWhitelist) internal {
+        state = ActionState.Idle;
+        opynWhitelist = IWhitelist(_opynWhitelist);
+    }
 
-  function _initRollOverBase(address _opynWhitelist) internal {
-    state = ActionState.Idle;
-    opynWhitelist = IWhitelist(_opynWhitelist);
-  }
+    /**
+     * owner can commit the next otoken, if it's in idle state.
+     * or re-commit it if needed during the commit phase.
+     */
+    function commitOToken(address _nextOToken) external onlyOwner {
+        require(state != ActionState.Activated, "R3");
+        _checkOToken(_nextOToken);
+        nextOToken = _nextOToken;
 
-  /**
-   * owner can commit the next otoken, if it's in idle state.
-   * or re-commit it if needed during the commit phase.
-   */
-  function commitOToken(address _nextOToken) external onlyOwner {
-    require(state != ActionState.Activated, "R3");
-    _checkOToken(_nextOToken);
-    nextOToken = _nextOToken;
+        state = ActionState.Committed;
 
-    state = ActionState.Committed;
-    
-    commitStateStart = block.timestamp;
-  }
+        commitStateStart = block.timestamp;
 
-  function _setActionIdle() internal {
-    onlyActivated();
-    // wait for the owner to set the next option
-    state = ActionState.Idle;
-  }
+        IOToken nextToken = IOToken(_nextOToken);
+        // Can override or replace this.
+        require(
+            nextToken.underlyingAsset(),
+            0xd0a1e359811322d97991e03f863a0c30c2cf029c, // weth
+            "S8"
+        );
+    }
 
-  function _rollOverNextOTokenAndActivate() internal {
-    onlyCommitted();
-    require(block.timestamp - commitStateStart > MIN_COMMIT_PERIOD, "R4");
+    function _setActionIdle() internal {
+        onlyActivated();
+        // wait for the owner to set the next option
+        state = ActionState.Idle;
+    }
 
-    otoken = nextOToken;
-    nextOToken = address(0);
+    function _rollOverNextOTokenAndActivate() internal {
+        onlyCommitted();
+        require(block.timestamp - commitStateStart > MIN_COMMIT_PERIOD, "R4");
 
-    state = ActionState.Activated;
-  }
+        otoken = nextOToken;
+        nextOToken = address(0);
 
-  function _checkOToken(address _nextOToken) private view {
-    require(opynWhitelist.isWhitelistedOtoken(_nextOToken), 'R5');
-    _customOTokenCheck(_nextOToken);
-  }
+        state = ActionState.Activated;
+    }
 
-  /**
-   * @dev funtion to add some custom logic to check the next otoken is valid to this strategy
-   * this hook is triggered while action owner calls "commitNextOption"
-   * so accessing otoken will give u the current otoken. 
-   */
-  function _customOTokenCheck(address _nextOToken) internal virtual view {
-  }
+    function _checkOToken(address _nextOToken) private view {
+        require(opynWhitelist.isWhitelistedOtoken(_nextOToken), "R5");
+        _customOTokenCheck(_nextOToken);
+    }
+
+    /**
+     * @dev funtion to add some custom logic to check the next otoken is valid to this strategy
+     * this hook is triggered while action owner calls "commitNextOption"
+     * so accessing otoken will give u the current otoken.
+     */
+    function _customOTokenCheck(address _nextOToken) internal view virtual {}
 }
