47282145bf43e8d85968fcffd04ede259d3868dd
Darya Kaviani <daryakavi@gmail.com>
2022-03-15 14:37:21 -0700

    fix weth address

--- contracts/utils/RollOverBase.sol
+++ contracts/utils/RollOverBase.sol
@@ -80,7 +80,7 @@ contract RollOverBase is Ownable {
         // Can override or replace this.
         require(
             nextToken.underlyingAsset(),
-            0xd0a1e359811322d97991e03f863a0c30c2cf029c, // weth
+            "0xd0a1e359811322d97991e03f863a0c30c2cf029c", // weth
             "S8"
         );
     }
